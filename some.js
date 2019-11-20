ddbc729faa92ee65998b8b71eb1448179782dec6
张丹丹 <zdd87550@163.com>
2019-11-20 11:21:31 +0800

    no message

--- some.js
+++ some.js
@@ -15,40 +15,84 @@
 
 
 
-const code =
-`<template>
-    <div class="default_code">
-        <input v-model="message">
-        {{ message }}
-        <div @click="test()">点击</div>
-    </div>
-</template>
-<script>
-    export default {
-        data () {
-            return {
-                message: '',
-                percent1: 30,
-            }
-        },
-        methods:{
-            test(){
-                this.$vux.toast.text('hello', 'top')
-            }
-        }
+// const code =
+// `<template>
+//     <div class="default_code">
+//         <input v-model="message">
+//         {{ message }}
+//         <div @click="test()">点击</div>
+//     </div>
+// </template>
+// <script>
+//     export default {
+//         data () {
+//             return {
+//                 message: '',
+//                 percent1: 30,
+//             }
+//         },
+//         methods:{
+//             test(){
+//                 this.$vux.toast.text('hello', 'top')
+//             }
+//         }
       
-    }
-</script>
-<style lang="scss">
-    .default_code{
-        border:1px solid #eaeaea;
-        padding:20px 30px;
-    }
-    input{
-        padding:15px 20px;
-        border:1px solid red;
-        border-radius:3px;
-    }
-</style>`
+//     }
+// </script>
+// <style lang="scss">
+//     .default_code{
+//         border:1px solid #eaeaea;
+//         padding:20px 30px;
+//     }
+//     input{
+//         padding:15px 20px;
+//         border:1px solid red;
+//         border-radius:3px;
+//     }
+// </style>`
 
-export default code
+// export default code
+
+
+; (function () {
+    var defaultCode =
+    `<template>
+        <div class="default_code">
+            <input v-model="message">
+            {{ message }}
+            <div @click="test()">点击</div>
+        </div>
+    </template>
+    <script>
+        export default {
+            data () {
+                return {
+                    message: '',
+                    percent1: 30,
+                }
+            },
+            methods:{
+                test(){
+                    this.$vux.toast.text('hello', 'top')
+                }
+            }
+        
+        }
+    </script>
+    <style lang="scss">
+        .default_code{
+            border:1px solid #eaeaea;
+            padding:20px 30px;
+        }
+        input{
+            padding:15px 20px;
+            border:1px solid red;
+            border-radius:3px;
+        }
+    </style>`
+    if (typeof module !== 'undefined' && typeof exports === 'object') {
+        module.exports = defaultCode;
+    } else {
+        window.defaultCode = defaultCode;
+    }
+})
